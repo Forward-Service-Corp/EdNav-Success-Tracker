f81056c218975869eb770bc0e0965b4bfde36616
uxmccauley <dmccauley@fsc-corp.org>
2025-05-22 03:44:33 -0500

    Set default navigator value to 'All' in dropdown
    Updated the dropdown selector to default to 'All' when no navigator is selected. This ensures better handling of undefined or null navigator data, improving the user experience.

--- components/NavigatorSelector.jsx
+++ components/NavigatorSelector.jsx
@@ -1,31 +1,37 @@
 "use client";
-import { useNavigator } from '/contexts/NavigatorsContext';
+import { useNavigator } from "/contexts/NavigatorsContext";
 
 function NavigatorSelector() {
-  const { selectedNavigator, setSelectedNavigator, navigatorList } = useNavigator();
+  const { selectedNavigator, setSelectedNavigator, navigatorList } =
+    useNavigator();
 
   return (
     <div className="w-[180px]" style={{ zIndex: 10 }}>
-      <label htmlFor="theme-select" className="text-sm">Navigator:
-      <select
-        id="navigator-select"
-        value={selectedNavigator?.name || ''}
-        onChange={(e) => {
-          setSelectedNavigator(() => {
-            return navigatorList.find((nav) => nav?.name === e.target.value) || 'All';
-          });
-        }}
-        className="select capitalize mt-1">
-        <option value="" disabled>
-          Select a navigator
-        </option>
-        <option value="All">All</option>
-        {navigatorList.map((nav, index) => (
-          <option key={index} value={nav?.name || 'All'}>
+      <label htmlFor="theme-select" className="text-sm">
+        Navigator:
+        <select
+          id="navigator-select"
+          value={selectedNavigator?.name || "All"}
+          onChange={(e) => {
+            setSelectedNavigator(() => {
+              return (
+                navigatorList.find((nav) => nav?.name === e.target.value) ||
+                "All"
+              );
+            });
+          }}
+          className="select mt-1 capitalize"
+        >
+          <option value="" disabled>
+            Select a navigator
+          </option>
+          <option value="All">All</option>
+          "All"vigatorList.map((nav, index) => (
+          <option key={index} value={(nav && nav?.name) || "All"}>
             {nav?.name}
           </option>
-        ))}
-      </select>
+          ))}
+        </select>
       </label>
     </div>
   );
