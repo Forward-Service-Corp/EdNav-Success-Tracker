a168ab3c63cb23358683eb1c07c805864ea6a405
uxmccauley <dmccauley@fsc-corp.org>
2025-05-22 10:57:42 -0500

    Update date handling and editing functionality
    Replaced `null` with an empty string as the initial state for `dateValue` and introduced a date input field for better usability. Added "Save" and "Cancel" button logic to manage date edits effectively, improving user interaction for editing dates in the unlockable section.

--- components/ClientProfileUnlockableSection.jsx
+++ components/ClientProfileUnlockableSection.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import moment from 'moment';
-import { useClient } from '../contexts/ClientContext';
+import React, { useState } from "react";
+import moment from "moment";
+import { useClient } from "../contexts/ClientContext";
 
 function ClientProfileUnlockableSection({ isNarrow, section }) {
   const { selectedClient } = useClient();
-  const [dateValue, setDateValue] = useState(null);
+  const [dateValue, setDateValue] = useState("");
   const [editDate, setEditDate] = useState(false);
 
   function hasValidKey(obj, key) {
@@ -14,27 +14,32 @@ function ClientProfileUnlockableSection({ isNarrow, section }) {
   return (
     <div id={section} className={`relative`}>
       <div
-        className={`absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center ${selectedClient[section]?.completed === true ? 'invisible' : 'visible'}`}>
+        className={`absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center ${selectedClient[section]?.completed === true ? "invisible" : "visible"}`}
+      >
         <div
-          className={`text-base-content bg-base-300/70 m-auto max-w-3/4 rounded-lg p-4 md:p-6 text-center shadow text-sm md:text-base`}>
+          className={`text-base-content bg-base-300/70 m-auto max-w-3/4 rounded-lg p-4 text-center text-sm shadow md:p-6 md:text-base`}
+        >
           Add an activity regarding {section} to activate this area.
         </div>
       </div>
       <div
-        className={`bg-base-200 border-base-content/10 relative   rounded-lg border-1 p-3 md:p-6 shadow ${selectedClient[section] ? '' : 'opacity-50 blur-[2px]'}`}>
-        <div className={`${isNarrow ? 'text-lg' : 'text-xl'} uppercase font-light tracking-wider`}>{section}</div>
-        <div className="flex items-start gap-2 md:gap-3 flex-col md:flex-row">
+        className={`bg-base-200 border-base-content/10 relative rounded-lg border-1 p-3 shadow md:p-6 ${selectedClient[section] ? "" : "opacity-50 blur-[2px]"}`}
+      >
+        <div
+          className={`${isNarrow ? "text-lg" : "text-xl"} font-light tracking-wider uppercase`}
+        >
+          {section}
+        </div>
+        <div className="flex flex-col items-start gap-2 md:flex-row md:gap-3">
           <div className={`w-full md:w-1/2`}>
             <div className={`mb-2 flex items-end gap-2`}>
-              {hasValidKey(selectedClient[section], 'referralDate') ? (
+              {hasValidKey(selectedClient[section], "referralDate") ? (
                 <div>
                   <div className={`text-base-content/60 font-medium`}>
                     Referral Date
                   </div>
                   <div className={`text-sm md:text-base`}>
-                    {moment(
-                      selectedClient[section]?.referralDate
-                    ).calendar()}
+                    {moment(selectedClient[section]?.referralDate).calendar()}
                   </div>
                 </div>
               ) : (
@@ -49,13 +54,11 @@ function ClientProfileUnlockableSection({ isNarrow, section }) {
           </div>
           <div className={`w-full md:w-1/2`}>
             <div className={`mb-2 flex items-end gap-2`}>
-              {hasValidKey(selectedClient[section], 'completionDate') ? (
+              {hasValidKey(selectedClient[section], "completionDate") ? (
                 <div>
                   <div className={`font-medium`}>Completion Date</div>
                   <div className={`text-sm md:text-base`}>
-                    {moment(
-                      selectedClient[section]?.completionDate
-                    ).calendar()}
+                    {moment(selectedClient[section]?.completionDate).calendar()}
                   </div>
                 </div>
               ) : (
@@ -69,11 +72,35 @@ function ClientProfileUnlockableSection({ isNarrow, section }) {
             </div>
           </div>
         </div>
-        <button onClick={() => {
-          setEditDate(true);
-        }} className={`btn btn-outline btn-info btn-xs md:btn-sm mt-4 md:mt-6 mb-1`}>
-          Edit Dates
-        </button>
+        <input
+          type="date"
+          name={`achievementDate`}
+          value={dateValue}
+          id={`achievementDate`}
+          className={`input input-sm ${!editDate ? "hidden" : "visible"}`}
+          onChange={(e) => {
+            setDateValue(e.target.value);
+          }}
+        />
+        <div className={`flex flex-row gap-2 md:flex-col md:gap-3`}>
+          <button
+            onClick={() => {
+              setEditDate(!editDate);
+            }}
+            className={`btn btn-outline btn-info btn-xs md:btn-sm mt-4 mb-1 md:mt-6`}
+          >
+            {editDate ? "Cancel" : "Edit"}
+          </button>
+          <button
+            className={`btn btn-outline btn-sm ${editDate ? "visible" : "hidden"}`}
+            onClick={() => {
+              selectedClient[section].referralDate = dateValue;
+              selectedClient[section].completionDate = dateValue;
+            }}
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
